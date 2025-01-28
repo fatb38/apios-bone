c891bd797085a71764cc6355eb1b657163a11422
fatb38 <arno.tgh@gmail.com>
2025-01-28 21:34:07 +0100

    latest feedback

---
+++ src/content/communication/communication-data.ts
@@ -0,0 +1,123 @@
+export const publications = [
+  {
+    title:
+      "3D-Printed Osteoinductive Polymeric Scaffolds with Optimized Architecture to Repair a Sheep Metatarsal Critical-Size Bone Defect.",
+    doi: "10.1002/adhm.202301692",
+  },
+  {
+    title:
+      "Additive Manufacturing of Material Scaffolds for Bone Regeneration: Toward Application in the Clinics.",
+    doi: "10.1002/adfm.202006967",
+  },
+  {
+    title:
+      "3D-printed scaffold combined to 2D osteoinductive coatings to repair a critical-size mandibular bone defect.",
+    doi: "10.1016/j.mtbio.2021.100113",
+  },
+  {
+    title:
+      "Surface delivery of tunable doses of BMP-2 from an adaptable polymeric scaffold induces volumetric bone regeneration.",
+    doi: "10.1016/j.biomaterials.2016.06.001",
+  },
+  {
+    title:
+      "Bone regeneration strategies: Engineered scaffolds, bioactive molecules and stem cells current stage and future perspectives.",
+    doi: "10.1016/j.biomaterials.2018.07.017",
+  },
+  {
+    title:
+      "Recent Developments in Layer-by-Layer Assembly for Drug Delivery and Tissue Engineering Applications.",
+    doi: "10.1002/adhm.202302713",
+  },
+  {
+    title:
+      "Assessment of a polyelectrolyte multilayer film coating loaded with BMP-2 on titanium and PEEK implants in the rabbit femoral condyle.",
+    doi: "10.1016/j.actbio.2016.03.010",
+  },
+  {
+    title:
+      "The stability of BMP loaded polyelectrolyte multilayer coatings on titanium.",
+    doi: "10.1016/j.biomaterials.2013.03.067",
+  },
+  {
+    title:
+      "Secondary structure of rhBMP-2 in a protective biopolymeric carrier material.",
+    doi: "10.1021/bm3010808",
+  },
+  {
+    title:
+      "The performance of BMP-2 loaded TCP/HAP porous ceramics with a polyelectrolyte multilayer film coating.",
+    doi: "10.1016/j.biomaterials.2011.06.062",
+  },
+  {
+    title:
+      "Layer-by-layer films as a biomimetic reservoir for rhBMP-2 delivery: controlled differentiation of myoblasts to osteoblasts.",
+    doi: "10.1002/smll.200800804",
+  },
+  {
+    title:
+      "Improvement of stability and cell adhesion properties of polyelectrolyte multilayer films by chemical cross-linking.",
+    doi: "10.1021/bm0342281",
+  },
+];
+
+export const patents = [
+  {
+    title: "Implantable medical device for bone repair.",
+    number: "WO2022063771A1",
+    linkUrl: "https://patents.google.com/patent/WO2022063771A1",
+  },
+  {
+    title:
+      "Process for preparing a surface coated by crosslinked polyelectrolyte multilayer films as a biomimetic reservoir for proteins.",
+    number: "WO2010081884A2",
+    linkUrl: "https://patents.google.com/patent/WO2010081884A2",
+  },
+];
+
+export const medias = [
+  {
+    type: "Press release",
+    linkTitle: "Le Monde",
+    linkUrl:
+      "https://www.lemonde.fr/sante/article/2024/09/06/quand-une-ia-imite-la-nature-pour-mieux-reparer-les-os_6305261_1651302.html",
+  },
+  {
+    type: "Press release",
+    linkTitle: "Blog CNRS",
+    linkUrl:
+      "https://lejournal.cnrs.fr/nos-blogs/focus-sciences/un-implant-bioactif-imprime-en-3d-revolutionne-la-reparation-osseuse",
+  },
+  {
+    type: "Podcast",
+    linkTitle: "La chronique Santé de Thierry Lhermitte",
+    linkUrl:
+      "https://www.radiofrance.fr/franceinter/podcasts/la-chronique-sante-de-thierry-lhermitte/la-chronique-sante-de-thierry-lhermitte-du-lundi-18-novembre-2024-8484350",
+  },
+  {
+    type: "Press release",
+    linkTitle: "FRM",
+    linkUrl:
+      "https://www.frm.org/fr/actualites/debat-comment-la-recherche-medicale-s-inspire-de-la-nature",
+  },
+  {
+    type: "Press release",
+    linkTitle: "Femme Actuelle",
+    linkUrl: "https://www.femmeactuelle.fr/sante/sante-pratique/biomimetisme-6-innovations-medicales-inspirees-de-la-nature-2178570",
+  },
+  {
+    type: "Press release",
+    linkTitle: "Polytechnique Insights",
+    linkUrl: "https://www.polytechnique-insights.com/tribunes/science/quand-la-medecine-regeneratrice-imite-la-nature/",
+  },
+  {
+    type: "Video",
+    linkTitle: "Midi Minatec 2023",
+    linkUrl: "https://www.youtube.com/watch?v=JtKpDyFzKk4",
+  },
+  {
+    type: "Video",
+    linkTitle: "TEDxGrenoble",
+    linkUrl: "https://www.youtube.com/watch?v=gk7X5gbcz_w",
+  },
+];
